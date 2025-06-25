3e12280a546a1d1160730811f000e44923e101d8
officialsantos <sanchu-78@hotmail.com>
2025-06-25 10:53:41 -0300

    Ahora se envian correos al hacer compras

--- sales.php
+++ sales.php
@@ -1,12 +1,14 @@
 <?php
 	include 'includes/session.php';
+	use PHPMailer\PHPMailer\PHPMailer;
+	use PHPMailer\PHPMailer\Exception;
 
 	if(isset($_GET['pay'])){
 		$payid = $_GET['pay'];
 		$date = date('Y-m-d');
 
 		$conn = $pdo->open();
-
+		
 		try{
 			
 			$stmt = $conn->prepare("INSERT INTO sales (user_id, pay_id, sales_date) VALUES (:user_id, :pay_id, :sales_date)");
@@ -18,19 +20,67 @@
 				$stmt->execute(['user_id'=>$user['id']]);
 
 				foreach($stmt as $row){
-					$stmt = $conn->prepare("INSERT INTO details (sales_id, product_id, quantity) VALUES (:sales_id, :product_id, :quantity)");
-					$stmt->execute(['sales_id'=>$salesid, 'product_id'=>$row['product_id'], 'quantity'=>$row['quantity']]);
+					$insert = $conn->prepare("INSERT INTO details (sales_id, product_id, quantity) VALUES (:sales_id, :product_id, :quantity)");
+					$insert->execute(['sales_id'=>$salesid, 'product_id'=>$row['product_id'], 'quantity'=>$row['quantity']]);
 				}
 
 				$stmt = $conn->prepare("DELETE FROM cart WHERE user_id=:user_id");
 				$stmt->execute(['user_id'=>$user['id']]);
 
-				$_SESSION['success'] = 'Transacción exitosa. Gracias.';
+				$_SESSION['success'] = 'Transacción exitosa. Gracias.<br> Se envió una notificación a su correo electrónico.';
+				
+				try{
+					$email = $user['email'];
+					$message = "
+						<h2>Gracias por realizar tu compra.</h2>
+						<p>Por seguridad, todos los datos son almacenados en tu perfil, en el Historial de Transacciones.</p>
+						<p>Puede cliquear en el enlace para ver sus datos automáticamente en nuestro sitio web.</p>
+						<a href='http://localhost/olimpiadas/profile.php?sales_id=".$salesid."'>Ver mi compra</a>
+					";
 
-			}
-			catch(PDOException $e){
-				$_SESSION['error'] = $e->getMessage();
-			}
+					//Load phpmailer
+		    		require 'vendor/autoload.php';
+
+		    		$mail = new PHPMailer(true);                             
+				    try {
+				        //Server settings
+				        $mail->isSMTP();                                     
+				        $mail->Host = 'smtp.gmail.com';                      
+				        $mail->SMTPAuth = true;                               
+				        $mail->Username = 'sanchuap@gmail.com';     
+				        $mail->Password = 'vksqwjxzbikzbfqo';                    
+				        $mail->SMTPOptions = array(
+				            'ssl' => array(
+				            'verify_peer' => false,
+				            'verify_peer_name' => false,
+				            'allow_self_signed' => true
+				            )
+				        );                         
+				        $mail->SMTPSecure = 'tls';                           
+				        $mail->Port = 587;                                   
+
+				        $mail->setFrom('sanchuap@gmail.com');
+				        
+				        //Recipients
+				        $mail->addAddress($email);              
+				        $mail->addReplyTo('sanchuap@gmail.com');
+				       
+				        //Content
+				        $mail->isHTML(true);                                  
+				        $mail->Subject = 'PatagoniaViajes - Compra exitosa';
+				        $mail->Body    = $message;
+
+				        $mail->send();
+
+				    } 
+				    catch (Exception $e) {
+				        $_SESSION['error'] = 'El mensaje no pudo ser enviado. Mailer Error: '.$mail->ErrorInfo;
+				    }
+				}
+				catch(PDOException $e){
+					$_SESSION['error'] = $e->getMessage();
+				}
+			
 
 		}
 		catch(PDOException $e){
@@ -39,7 +89,12 @@
 
 		$pdo->close();
 	}
-	
+ catch (PDOException $e1) {
+$_SESSION['error'] = $e1->getMessage();
+ }
+	}
+
+
 	header('location: profile.php');
 	
 ?>
