c5be801396afbc7f573e07440782be4bf24140a2
officialsantos <sanchu-78@hotmail.com>
2025-06-28 06:14:28 -0300

    Conectada DB al host gratuito

--- includes/conn.php
+++ includes/conn.php
@@ -1,30 +1,36 @@
 <?php
 
-Class Database{
- 
-	private $server = "sql107.infinityfree.com";
+class Database {
+
+	private $host = "sql107.infinityfree.com";
+	private $dbname = "if0_39331618_ecomm"; // Reemplazá con el nombre correcto de tu base de datos
 	private $username = "if0_39331618";
 	private $password = "GewsKfWastFpra";
-	private $options  = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,);
+	private $options  = array(
+		PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+		PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+	);
 	protected $conn;
- 	
-	public function open(){
- 		try{
- 			$this->conn = new PDO($this->server, $this->username, $this->password, $this->options);
- 			return $this->conn;
- 		}
- 		catch (PDOException $e){
- 			echo "There is some problem in connection: " . $e->getMessage();
- 		}
- 
-    }
- 
-	public function close(){
-   		$this->conn = null;
- 	}
- 
+
+	public function open() {
+		try {
+			$this->conn = new PDO(
+				"mysql:host={$this->host};dbname={$this->dbname};charset=utf8",
+				$this->username,
+				$this->password,
+				$this->options
+			);
+			return $this->conn;
+		} catch (PDOException $e) {
+			echo "Error de conexión: " . $e->getMessage();
+		}
+	}
+
+	public function close() {
+		$this->conn = null;
+	}
 }
 
 $pdo = new Database();
- 
+
 ?>
