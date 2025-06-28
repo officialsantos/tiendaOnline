6386c88a4521e51f85f3718b98456b670d960953
officialsantos <sanchu-78@hotmail.com>
2025-06-28 03:46:35 -0300

    Modificada la conexión a DB

--- includes/conn.php
+++ includes/conn.php
@@ -2,9 +2,9 @@
 
 Class Database{
  
-	private $server = "mysql:host=localhost;dbname=ecomm";
-	private $username = "root";
-	private $password = "";
+	private $server = "sql107.infinityfree.com";
+	private $username = "if0_39331618";
+	private $password = "GewsKfWastFpra";
 	private $options  = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,);
 	protected $conn;
  	
