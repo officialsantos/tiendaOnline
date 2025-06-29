d655dd08044b1155afec2b7046bb51d90d473abf
Jean-Pierre001 <Agustin500cm@gmail.com>
2025-06-29 15:49:02 -0300

    se agrego la ayuda en linea para los clientes

--- includes/footer.php
+++ includes/footer.php
@@ -4,5 +4,8 @@
         <b>All rights reserved</b>
       </div>
       <strong>Copyright &copy; 2018 <a href="https://www.sourcecodester.com/">SourceCodeSter</a></strong>
+      <?php 
+            include 'help_line.html';
+      ?>
     </div>
 </footer>
